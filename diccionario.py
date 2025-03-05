e2299143d2709e8d30d9956a455f496a6d1c6768
Chinyiskan <david.camilo.charry@gmail.com>
2025-03-05 12:07:03 -0500

    primer commit

--- diccionario.py
+++ diccionario.py
@@ -4,10 +4,3 @@ meme_dict = {
             }
 
 word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-
-#if word in meme_dict.keys():
-    # ¿Qué debemos hacer si se encuentra la palabra?
-#else:
-    # ¿Qué hacer si no se encuentra la palabra?
-
-print("Hola!")
